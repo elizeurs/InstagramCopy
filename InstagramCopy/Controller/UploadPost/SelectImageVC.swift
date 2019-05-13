37279e8a2bb3f33ad2c15ce49234a619de1e2562
elizeurs <13ders@gmail.com>
2019-05-13 01:59:58 -0300

    22. Import Photos from Library

--- InstagramCopy/Controller/UploadPost/SelectImageVC.swift
+++ InstagramCopy/Controller/UploadPost/SelectImageVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Photos
 
 private let reuseIdentifier = "SelectPhotoCell"
 private let headerIdentifier = "SelectPhotoHeader"
@@ -14,18 +15,25 @@ private let headerIdentifier = "SelectPhotoHeader"
 class SelectImageVC: UICollectionViewController, UICollectionViewDelegateFlowLayout {
   
   //  MARK: - Properties
+  var images = [UIImage]()
+  var assets = [PHAsset]()
+  var selectedImage: UIImage?
+  
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-//    register cell classes
+    //    register cell classes
     collectionView?.register(SelectPhotoCell.self, forCellWithReuseIdentifier: reuseIdentifier)
     collectionView?.register(SelectPhotoHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerIdentifier)
     
     collectionView?.backgroundColor = .white
     
-//    configure nav buttons
+    //    configure nav buttons
     configureNavigationButtons()
+    
+    //    fetch photos
+    fetchPhotos()
   }
   
   //  MARK: - UICollectionViewFlowLayout
@@ -55,12 +63,31 @@ class SelectImageVC: UICollectionViewController, UICollectionViewDelegateFlowLay
   }
   
   override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return 5
+    return images.count
   }
   
   override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
     
     let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerIdentifier, for: indexPath) as! SelectPhotoHeader
+    
+    if let selectedImage = self.selectedImage {
+
+//      index selected image
+      if let index = self.images.index(of: selectedImage) {
+        
+//      asset associated with selected image
+        let selectedAsset = self.assets[index]
+        
+        let imageManager = PHImageManager.default()
+        let targetSize = CGSize(width: 600, height: 600)
+        
+//        request image
+        imageManager.requestImage(for: selectedAsset, targetSize: targetSize, contentMode: .default, options: nil) { (image, info) in
+          
+          header.photoImageView.image = image
+        }
+      }
+    }
     
     return header
     
@@ -69,8 +96,18 @@ class SelectImageVC: UICollectionViewController, UICollectionViewDelegateFlowLay
   override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! SelectPhotoCell
     
-    return cell
+    cell.photoImageView.image = images[indexPath.row]
     
+    return cell
+  }
+  
+  override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    
+    self.selectedImage = images[indexPath.row]
+    self.collectionView?.reloadData()
+    
+    let indexPath = IndexPath(item: 0, section: 0)
+    collectionView.scrollToItem(at: indexPath, at: .bottom, animated: true)
   }
   
   //  MARK: - Handlers
@@ -88,5 +125,69 @@ class SelectImageVC: UICollectionViewController, UICollectionViewDelegateFlowLay
     navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
     
     navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .plain, target: self, action: #selector(handleNext))
+  }
+  
+  func getAssetFetchOptions() -> PHFetchOptions {
+    
+    let options = PHFetchOptions()
+    
+    //    fetch limit
+    options.fetchLimit = 30
+    
+    //    sort photos by date
+    let  sortDescriptor = NSSortDescriptor(key: "creationDate", ascending: false)
+    
+    //    set sort descriptor for options
+    options.sortDescriptors = [sortDescriptor]
+    
+    //    return options
+    return options
+  }
+  
+  func fetchPhotos() {
+    
+    let allPhotos = PHAsset.fetchAssets(with: .image, options: getAssetFetchOptions())
+    
+    print("Function running")
+    
+    //    fetch images on background thread
+    DispatchQueue.global(qos: .background).async {
+      
+      //      enumerate objects
+      allPhotos.enumerateObjects({ (asset, count, stop) in
+        
+        let imageManager = PHImageManager.default()
+        let targetSize = CGSize(width: 200, height: 200)
+        let options = PHImageRequestOptions()
+        options.isSynchronous = true
+        
+        //        request image representation for specified asset
+        imageManager.requestImage(for: asset, targetSize: targetSize, contentMode: .aspectFit, options: options, resultHandler: { (image, info) in
+          
+          if let image = image {
+            
+            //            append image to data source
+            self.images.append(image)
+            
+            //            append asset to data source
+            self.assets.append(asset)
+            
+            //            set selected image
+            if self.selectedImage == nil {
+              self.selectedImage = image
+            }
+            
+            //            reload collection view with images once count has completed
+            if count == allPhotos.count - 1 {
+              
+//              reload collection view on main thread
+              DispatchQueue.main.async {
+                self.collectionView?.reloadData()
+              }
+            }
+          }
+        })
+      })
+    }
   }
 }
